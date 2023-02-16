eafbe08f6c1a9033281438717a9b765d5bc39a83
Santiago <slponce@fi.uba.ar>
2023-02-16 09:45:49 -0300

    ejercicio finalizado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-const preguntas= document.querySelectorAll(".faq__question");
+const questions= document.querySelectorAll(".faq__question");
 
-
-preguntas.forEach(pregunta=>{
-    pregunta.addEventListener("click",(e)=>{
-    e.target.parentElement.lastElementChild.classList.toggle("hidden")
+questions.forEach(question=>{
+    question.addEventListener("click",(e)=>{
+    e.target.parentElement.lastElementChild.classList.toggle("hidden");
+    e.target.classList.toggle("faq__question--active")
     });
 }
 );
+
